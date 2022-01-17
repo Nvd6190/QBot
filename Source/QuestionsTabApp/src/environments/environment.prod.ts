157d5ebf9834ade816d385f3990852044cfbf8c8
Nvd6190 <41731913+Nvd6190@users.noreply.github.com>
2022-01-17 13:45:51 +0530

    Update environment.prod.ts

--- Source/QuestionsTabApp/src/environments/environment.prod.ts
+++ Source/QuestionsTabApp/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://qbot-testing.azurewebsites.net/api/Request/",
+    selfUrl: "https://qbot-testing-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "59304d84-3df0-45e2-8834-de4654ae8758",
+        clientId: "65a011fb-6a74-4f3b-ab89-f21f100e5fc6",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
